bb48e316ca6f49bf5674d65aca4ecfce4d08f7ef
Luu Huyen <91403067+luhuyn@users.noreply.github.com>
2023-07-12 02:28:33 +0700

    update Home.jsx login-form

--- State&LifeCycleMethods/login-form/src/components/Home.jsx
+++ State&LifeCycleMethods/login-form/src/components/Home.jsx
@@ -1,20 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 function Home({ onLogout }) {
-  useEffect(() => {
-    return () => {
-      alert('Goodbye!');
-    };
-  }, []);
-
   return (
     <div className="container d-flex align-items-center text-center">
       <div className="form-signin">
         <h1 className="h3 mb-3 fw-normal">Welcome</h1>
-        <button className="w-100 btn btn-lg btn-primary" type="button" onClick={onLogout}>Log out</button>
+        <button className="w-100 btn btn-lg btn-primary" type="button" onClick={onLogout}>
+          Log out
+        </button>
       </div>
     </div>
   );
 }
 
 export default Home;
+
